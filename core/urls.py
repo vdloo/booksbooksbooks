cae3548ab153367a29545e5ac0567d39683e556e
Rick van de Loo <rickvandeloo@gmail.com>
2015-12-26 20:25:15 +0100

    only redirect / not /.*$

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^.*$', RedirectView.as_view(url='admin/REST_API/book/', permanent=False), name='index'),
+    url(r'^$', RedirectView.as_view(url='admin/REST_API/book/', permanent=False), name='index'),
     url(r'^api/', include('REST_API.urls')),
 ]
