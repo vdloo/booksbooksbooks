29f85a76604f5a0aa1f2d8747dd445657b0c265b
Rick van de Loo <rickvandeloo@gmail.com>
2016-01-01 13:38:51 +0100

    allow importing with missing Author name
    fill the field with "Unknown Author" by default

--- REST_API/models.py
+++ REST_API/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Book(models.Model):
     create = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=100, default='Unknown Author')
     year = models.PositiveSmallIntegerField(blank=True, null=True)
     extension = models.CharField(max_length=100)
     path = models.CharField(max_length=255)
