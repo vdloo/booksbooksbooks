66a2b2cd27a99cef14c0661a79f6377bb78ae956
Rick van de Loo <rickvandeloo@gmail.com>
2016-01-01 12:24:12 +0100

    author and title can't be blank or null

--- REST_API/models.py
+++ REST_API/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Book(models.Model):
     create = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, default='')
-    author = models.CharField(max_length=100, default='')
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField(blank=True, null=True)
     path = models.CharField(max_length=255)
 
