fe48c71563374c2729d823eeced58703ce1068d6
Rick van de Loo <rickvandeloo@gmail.com>
2022-01-11 10:59:12 +0100

    add jquery into the django admin
    no longer in django by default

--- core/admin.py
+++ core/admin.py
@@ -3,13 +3,19 @@ from REST_API.models import Book
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('author', 'title', 'year', 'extension')
     list_filter = ('extension',)
     search_fields = ('author', 'title')
+
     class Media:
-        js = ('js/utils.js',)
+        js = (
+            '//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js',
+            'js/utils.js'
+        )
         css = {'all': ('css/hide_admin.css',)}
+
 
 admin.site.register(Book, BookAdmin)
 admin.site.unregister(User)
